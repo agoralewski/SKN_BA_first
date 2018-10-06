af81aab7116460f3475c4fb6f1d7101178c26429
Adrianna Napiórkowska <napiorkowska.adrianna@gmail.com>
2018-10-06 23:22:55 +0200

    Add pipeline file (read data)

---
+++ Main.py
@@ -0,0 +1,17 @@
+#Imports
+import pandas as pd
+
+#Parameters
+filelocation = 'C:/datasets'
+
+#Getting Data
+train = pd.read_csv(filelocation+'/train.csv')
+print(train.shape)
+print(train.head(5))
+
+#Data transformation
+
+#Model
+
+#Export results to submission file
+
