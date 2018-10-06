33d8d6ced59afad4c91b884d5170cc9f275b85e0
Adrianna Napiórkowska <napiorkowska.adrianna@gmail.com>
2018-10-07 00:56:58 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,3 +16,6 @@ print(train.head(5))
 #Model Evaluation
 
 #Export results to submission file
+#submission = test[['key_id', 'word']]
+#submission.to_csv('first_submission.csv', index=False)
+#submission.head()
