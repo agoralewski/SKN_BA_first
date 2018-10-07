b57f39f72c052d81dc8404f8e94e4e161c172ad1
Adrianna Napiórkowska <napiorkowska.adrianna@gmail.com>
2018-10-07 03:02:25 +0200

    add code: create test set

--- Main.py
+++ Main.py
@@ -1,13 +1,27 @@
 #Imports
+import os
 import pandas as pd
 
 #Parameters
-filelocation = 'C:/datasets'
+filelocation = 'C:/datasets/'
 
 #Getting Data
-train = pd.read_csv(filelocation+'/train.csv')
-print(train.shape)
+train_files = os.listdir(filelocation+"/train_simplified/")
+print(train_files[:5])
+columns = ['countrycode', 'drawing', 'key_id', 'recognized', 'timestamp', 'word']
+selected_categories=['airplan', 'axe' 'book' 'bowtie', 'cake' 'calculator']
+
+train = pd.read_csv(filelocation + 'train_simplified/' + train_files[0]).head(1000)
+for file in train_files[1:61]:
+    sub_train=pd.read_csv(filelocation + 'train_simplified/' + file).head(1000)
+    train = train.append(sub_train)
+    print(train.shape)
+
+print('!!! END !!! Train Test Size:', train.shape)
 print(train.head(5))
+
+### Filter for selected categories
+train = train.loc[train['word'].isin(selected_categories)]
 
 #Data transformation
 
