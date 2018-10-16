0b667cf118b7b9c16caa544a86af756ab2245365
Adrianna Napiórkowska <napiorkowska.adrianna@gmail.com>
2018-10-16 23:56:00 +0200

    add number of train cases per class as parameter

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import os
 
 #Parameters
 filelocation = mc.DATA_SET_DIR_PATH
+nb_training_cases_per_cat = 100
 
 #Getting Data
 train_files = os.listdir(filelocation+"train_simplified/")
@@ -12,9 +13,9 @@ print(train_files[:5])
 columns = ['countrycode', 'drawing', 'key_id', 'recognized', 'timestamp', 'word']
 selected_categories=['airplane', 'axe' 'book' 'bowtie', 'cake' 'calculator']
 
-train = pd.read_csv(filelocation + 'train_simplified/' + train_files[0]).head(1000)
+train = pd.read_csv(filelocation + 'train_simplified/' + train_files[0]).head(nb_training_cases_per_cat)
 for file in train_files[1:61]:
-    sub_train=pd.read_csv(filelocation + 'train_simplified/' + file).head(1000)
+    sub_train=pd.read_csv(filelocation + 'train_simplified/' + file).head(nb_training_cases_per_cat)
     train = train.append(sub_train)
     print(train.shape)
 
@@ -22,7 +23,7 @@ print('!!! END !!! Train Test Size:', train.shape)
 print(train.head(5))
 
 ### Filter for selected categories
-train = train.loc[train['word'].isin(selected_categories)]
+train_selected = train.loc[train['word'].isin(selected_categories)]
 
 #Data transformation
 
