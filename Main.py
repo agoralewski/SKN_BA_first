37778e842edd6499e1f01a2a8ee72e8cb28c34c9
Adrianna Napiórkowska <napiorkowska.adrianna@gmail.com>
2018-10-27 12:41:16 +0200

    fix mising commas and add import for selected categories

--- Main.py
+++ Main.py
@@ -6,15 +6,19 @@ import os
 #Parameters
 filelocation = mc.DATA_SET_DIR_PATH
 nb_training_cases_per_cat = 100
+RunForSelectedCategories = True
 
 #Getting Data
 train_files = os.listdir(filelocation+"train_simplified/")
 print(train_files[:5])
 columns = ['countrycode', 'drawing', 'key_id', 'recognized', 'timestamp', 'word']
-selected_categories=['airplane', 'axe' 'book' 'bowtie', 'cake' 'calculator']
+selected_categories=['airplane', 'axe', 'book', 'bowtie', 'cake', 'calculator']
+
+if RunForSelectedCategories:
+    train_files = [x+'.csv' for x in selected_categories]
 
 train = pd.read_csv(filelocation + 'train_simplified/' + train_files[0], nrows=nb_training_cases_per_cat)
-for file in train_files[1:10]:
+for file in train_files[1:]:
     sub_train=pd.read_csv(filelocation + 'train_simplified/' + file, nrows=nb_training_cases_per_cat)
     sub_train = sub_train[sub_train.recognized==True]
     train = train.append(sub_train)
