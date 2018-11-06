2dde6e838587ef8080bde229b99fb86a193a462c
agoralewski <1arturro1@gmail.com>
2018-11-07 00:58:07 +0100

    add function - create_dummy_submission

--- Main.py
+++ Main.py
@@ -1,16 +1,32 @@
 #Imports
 from config import main_config as mc #user specific config
 import pandas as pd
+import numpy as np
 import os
+import datetime
 
 #Parameters
 filelocation = mc.DATA_SET_DIR_PATH
 nb_training_cases_per_cat = 100
 RunForSelectedCategories = True
 
+
+def create_dummy_submission(dataset_location, num_to_class):
+    """ creates submission with random chosen classes assigned to test drawings """
+    test_simplified_df = pd.read_csv(dataset_location+"test_simplified.csv")
+    submission = test_simplified_df[['key_id']]
+    ids = np.random.randint(0, len(num_to_class), 3)
+    submission['word'] = " ".join([num_to_class[k] for k in ids if k in num_to_class])
+    submission.to_csv('submissions/submission_{}.csv'.format(datetime.date.today()),
+                      index=False,
+                      #header=['key_id', 'word']
+                      )
+
+
 #Getting Data
 train_files = os.listdir(filelocation+"train_simplified/")
-print(train_files[:5])
+num_to_class = {i: v[:-4].replace(" ", "_") for i, v in enumerate(train_files)} #adds underscores
+
 columns = ['countrycode', 'drawing', 'key_id', 'recognized', 'timestamp', 'word']
 selected_categories=['airplane', 'axe', 'book', 'bowtie', 'cake', 'calculator']
 
@@ -37,6 +53,5 @@ print(train.head(5))
 #Model Evaluation
 
 #Export results to submission file
-#submission = test[['key_id', 'word']]
-#submission.to_csv('first_submission.csv', index=False)
-#submission.head()
+create_dummy_submission(filelocation, num_to_class)
+
