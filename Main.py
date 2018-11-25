c88e8c4c9889e702923f47c27f0e8831ade9473c
agoralewski <1arturro1@gmail.com>
2018-11-25 22:52:46 +0100

    new read_train_simplified_csv function and some minor changes

--- Main.py
+++ Main.py
@@ -15,14 +15,16 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 # Parameters
-filelocation = mc.DATA_SET_DIR_PATH
-nb_training_cases_per_cat = 100
-RunForSelectedCategories = True
+files_location = mc.DATA_SET_DIR_PATH
+nb_training_cases_per_cat = 300
+run_selected_categories = True
+NUM_SHUFFLED_CSV = 100
 
 
-def create_dummy_submission(dataset_location, num_to_class):
+# functions
+def create_dummy_submission(files_location, num_to_class):
     """ creates submission with random chosen classes assigned to test drawings """
-    test_simplified_df = pd.read_csv(dataset_location + "test_simplified.csv")
+    test_simplified_df = pd.read_csv(files_location + "test_simplified.csv")
     submission = test_simplified_df[['key_id']]
     # draw three ids of classes
     ids = np.random.randint(0, len(num_to_class), 3)  # TODO: here is sth wrong, maybe sb fix it:) hint: apple apple apple
@@ -65,11 +67,11 @@ def create_bitmap(input_json, bitmap_height=256, bitmap_width=256):
     return np.array(image) / 255.
 
 
-def draw_raw_data_first_try():
+def draw_raw_data_first_try(files_location):
     """ first try analyze raw data - drawings from Magda"""
     import ast
     import matplotlib.pyplot as plt
-    test_raw = pd.read_csv(filelocation + "/test_raw.csv", index_col="key_id", nrows=100)
+    test_raw = pd.read_csv(files_location + "/test_raw.csv", index_col="key_id", nrows=100)
 
     # first 10 drawings
     first_ten_ids = test_raw.iloc[:10].index
@@ -87,21 +89,45 @@ def create_classes_dictionary(train_files_names):
     return {i: v[:-4].replace(" ", "_") for i, v in enumerate(train_files_names)}  # adds underscores
 
 
+def read_train_simplified_csv(files_location, category, nrows=None, drawing_transform=False):
+    """
+    read train simplified csv of specific category
+    Parameters
+    ----------
+    :param files_location: str
+    data set location
+    :param category: str
+    category of data to be read
+    :param nrows: int
+    number of rows to read form one file
+    :param drawing_transform: bool
+    True if drawing have to be interpret as JSON
+    Returns
+    -------
+    DataFrame : contains data from simplified csv file
+    """
+    df = pd.read_csv(os.path.join(files_location, 'train_simplified', category + '.csv'), nrows=nrows)
+    if drawing_transform:
+        df['drawing'] = df['drawing'].apply(json.loads)
+    return df
+
+
 # Getting Data
-train_files = os.listdir(filelocation + "train_simplified/")
+train_files = os.listdir(files_location + "train_simplified/")
+print(train_files)
 num_to_class = create_classes_dictionary(train_files)
 
 columns = ['countrycode', 'drawing', 'key_id', 'recognized', 'timestamp', 'word']
 selected_categories = ['airplane', 'axe', 'book', 'bowtie', 'cake', 'calculator']
 
-if RunForSelectedCategories:
+if run_selected_categories:
     train_files = [x + '.csv' for x in selected_categories]
     print(train_files)
 
-train = pd.read_csv(filelocation + 'train_simplified/' + train_files[0], nrows=nb_training_cases_per_cat)
+train = read_train_simplified_csv(files_location, train_files[0], nb_training_cases_per_cat)
 for file in train_files[1:]:
     print(file)
-    sub_train = pd.read_csv(filelocation + 'train_simplified/' + file, nrows=nb_training_cases_per_cat)
+    sub_train = read_train_simplified_csv(files_location, file, nrows=nb_training_cases_per_cat)
     sub_train = sub_train[sub_train.recognized]
     train = train.append(sub_train)
     print(train.shape)
@@ -133,8 +159,7 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 # Model Evaluation
 
 # Export results to submission file
-create_dummy_submission(filelocation, num_to_class)
-
+create_dummy_submission(files_location, num_to_class)
 
 
 
